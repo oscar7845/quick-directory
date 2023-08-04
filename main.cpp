df38eecf7584a1cde3a23b6563b4779575645b50
oscar7845 <oc59@njit.edu>
2023-08-04 10:24:32 +0200

    connect menu and mouse events

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <QApplication>
+#include <QSystemTrayIcon>
+#include <QStorageInfo>
 #include <QFileIconProvider>
+#include <QMenu>
 
 #include "pathmenu.h"
 
@@ -15,7 +18,29 @@ int main(int argc, char *argv[])
     QMenu trayMenu;
     QObject::connect(&trayMenu, &QMenu::aboutToShow, [&]()
     {
-        //
+        trayMenu.clear();
+
+        for (const auto &info : QStorageInfo::mountedVolumes()) {
+            PathMenu *pathMenu = new PathMenu(info.rootPath(), info.displayName());
+            pathMenu->setIcon(iconProvider.icon(QFileInfo(info.rootPath())));
+            trayMenu.addMenu(pathMenu);
+
+            QObject::connect(&trayMenu, &QMenu::aboutToHide, pathMenu, &QObject::deleteLater);
+        }
+
+        trayMenu.addSeparator();
+        trayMenu.addAction("&Settings..."); // TODO
+        trayMenu.addAction("&Quit", &app, &QApplication::quit);
     });
 
+    QObject::connect(&trayIcon, &QSystemTrayIcon::activated, [&](QSystemTrayIcon::ActivationReason reason)
+    {
+        (void)reason;
+
+        trayMenu.popup(QCursor::pos());
+    });
+
+    trayIcon.show();
+
+    return app.exec();
 }
