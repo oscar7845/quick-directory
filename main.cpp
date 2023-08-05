56ad6a6cfb2177e1b1f4e122cad9bfe537556e2b
oscar7845 <oc59@njit.edu>
2023-08-05 12:10:13 +0200

    taskbar icon and display option

--- main.cpp
+++ main.cpp
@@ -9,18 +9,38 @@
 int main(int argc, char *argv[])
 {
     QSettings::setDefaultFormat(QSettings::IniFormat);
-    QApplication::setOrganizationName("arturo182");
-    QApplication::setApplicationName("TrayBrowser");
+    QApplication::setOrganizationName("app");
+    QApplication::setApplicationName("QuickDir");
 
     QApplication app(argc, argv);
     app.setQuitOnLastWindowClosed(false);
 
-    QFileIconProvider iconProvider;
-
     QSystemTrayIcon trayIcon(&app);
-    trayIcon.setIcon(iconProvider.icon(QFileIconProvider::Folder));
 
     TrayMenu trayMenu;
+    QObject::connect(&trayMenu, &TrayMenu::settingsApplied, &trayMenu, [&]()
+    {
+        QSettings sett;
+
+        QFileIconProvider iconProvider;
+
+        switch (sett.value("iconSource", TrayIconSource::App).toInt()) {
+        case TrayIconSource::App:
+            trayIcon.setIcon(QIcon(":/res/icon.png"));
+            break;
+
+        case TrayIconSource::Os:
+            trayIcon.setIcon(iconProvider.icon(QFileIconProvider::Folder));
+            break;
+
+        case TrayIconSource::User:
+            trayIcon.setIcon(QIcon(sett.value("iconFile").toString()));
+            break;
+
+        default:
+            break;
+        }
+    });
     QObject::connect(&trayIcon, &QSystemTrayIcon::activated, &trayIcon, [&](QSystemTrayIcon::ActivationReason reason)
     {
         (void)reason;
